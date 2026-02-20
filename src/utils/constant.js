deecff77266dbb08d76f0068bf737da035ebac17
websyncdevteam <team.websync99@gmail.com>
2026-02-20 02:45:41 -0800

    Update constant.js

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,5 +1,7 @@
-export const USER_API_END_POINT = "http://localhost:3000/api/v1/user";
-export const JOB_API_END_POINT = "http://localhost:3000/api/v1/job";
-export const APPLICATION_API_END_POINT = "http://localhost:3000/api/v1/application";
-export const COMPANY_API_END_POINT = "http://localhost:3000/api/v1/company";
-export const ADMIN_API_END_POINT = "http://localhost:3000/api/v1/admin"; // 
+const API_BASE_URL = import.meta.env.VITE_API_URL || 'https://www.backendserver.aim9hire.com/api/v1';
+
+export const USER_API_END_POINT = `${API_BASE_URL}/user`;
+export const JOB_API_END_POINT = `${API_BASE_URL}/job`;
+export const APPLICATION_API_END_POINT = `${API_BASE_URL}/application`;
+export const COMPANY_API_END_POINT = `${API_BASE_URL}/company`;
+export const ADMIN_API_END_POINT = `${API_BASE_URL}/admin`;
